5c6aa2fcb3857559facc7ef987faf5264dfb958c
juhaszdominik <113378949+Dxminik21@users.noreply.github.com>
2022-10-09 22:09:32 +0200

    Add files via upload

---
+++ HF3.1.py
@@ -0,0 +1,19 @@
+def h(i):
+    value_a = input("Enter 'a' value:")
+    value_a = int(value_a)
+    value_b = input("Enter 'b' value:")
+    value_b = int(value_b)
+    print(value_a/value_b)
+
+i=0
+while i <=10:
+    try:
+        h(i)
+    except ZeroDivisionError:
+        print("Can't divide by zero")
+    finally:
+        print("Out of try except blocks")
+
+
+if __name__ == '__main__':
+    h()
